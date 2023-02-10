870dfe3a9a30b83d4820c04d6b45c32f5d7f951a
MSIPC <worgaming3@yandex.ru>
2023-02-11 00:50:10 +0300

    Нахождение корней квадратного уравнения зная их коэффициенты

--- 1lab/1lab.py
+++ 1lab/1lab.py
@@ -1,14 +1,13 @@
-import math
-b, a, c = map(int, input().split())
+from math import sqrt
+b, a, c = map(float, input().split())
 D = b ** 2 - 4 * a * c
 if a == 0 or b == 0 or c == 0:
     print('Корней нет')
-elif D < 0 or b == 0 or a == 0 or c == 0:
+elif D < 0:
     print('Корней нет')
 elif D == 0:
     print(f'{(-b) / (2 * a)}')
 else:
-    x1 = (-b + (math.sqrt(D) / 2 * a))
-    x2 = (-b - (math.sqrt(D) / 2 * a))
+    x1 = (-b + (sqrt(D) / 2 * a))
+    x2 = (-b - (sqrt(D) / 2 * a))
     print(round(x1, 1), round(x2, 1))
-
